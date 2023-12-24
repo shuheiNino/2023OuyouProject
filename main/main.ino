da67f782b845dc91ecfaa88b912c63e97890b3ba
shuheiNino <131248406+shuheiNino@users.noreply.github.com>
2023-12-24 16:00:45 +0900

    暫定完成版
    Xinputでpcにデータを送り、pcからarduinoまでシリアル通信でつなぎ、サーボが動くところまで確認済み

--- main/main.ino
+++ main/main.ino
@@ -1,14 +1,101 @@
 #include <Wire.h>
+#include <Servo.h>
+
+String cmds[4] = {"\0"}; // 分割された文字列を格納する配列 
+
+//ジョイコン値格納場所
+float LEFT_x_value = 0;
+float LEFT_y_value = 0;
+float RIGHT_x_value = 0;
+float RIGHT_y_value = 0;
+
+//miliseconds格納場所
+int Rud_val = 1500;
+int Ele_val = 1500;
+
+//サーボ初期設定
+Servo servoRudder;
+Servo servoElevator;
+float mid_servoRud = 1500;
+float mid_servoElevator = 1500;
+
+//スティックの感度調整(0~400)
+ int sensitivity_Rud = 400;
+ int sensitivity_Ele = 400;
 
 void setup()
 {
-  Wire.begin
+  Wire.begin();
   Serial.begin(9600);
-  auto sensor_BMX055 = Sensor_BMX055()
+  //auto sensor_BMX055 = Sensor_BMX055();
+
+  //servo　初期PIN設定
+  servoRudder.attach(3);
+  servoElevator.attach(5);
+  servoRudder.writeMicroseconds(mid_servoRud);
+  servoElevator.writeMicroseconds(mid_servoElevator);
 }
 
 void loop()
 {
-
+  Rud_val = calc_miliseconds(LEFT_x_value, mid_servoRud, sensitivity_Rud);
+  Ele_val = calc_miliseconds(RIGHT_y_value, mid_servoElevator, sensitivity_Ele);
+  servoRudder.writeMicroseconds(Rud_val);
+  servoElevator.writeMicroseconds(Ele_val);
+  Serial.print(Rud_val);
+  Serial.print(",");
+  Serial.println(Ele_val);
+  if(Serial.available() > 0) {
+    serEvent();
+  }
 }
+
+void serEvent(){
+  //割り込み処理のはず
+  String input = Serial.readStringUntil('\n');
+  //Serial.println(input);
+  int data_num = split(input, ',', cmds);
+  //Serial.println(cmds[0]);
+  LEFT_x_value = cmds[0].toFloat();
+  LEFT_y_value = cmds[1].toFloat();
+  RIGHT_x_value = cmds[2].toFloat();
+  RIGHT_y_value = cmds[3].toFloat();
+  
+  // cmds 配列の中身を空にする
+  for (int i = 0; i < 4; ++i) {
+    cmds[i] = "";
+  }
+}
+
+
+//split関数、コンマで値を分割
+int split(String data, char delimiter, String *dst){
+    int index = 0; 
+    int datalength = data.length();
+    
+    for (int i = 0; i < datalength; i++) {
+        char tmp = data.charAt(i);
+        if ( tmp == delimiter ) {
+            index++;
+        }
+        else dst[index] += tmp;
+    }
+    
+    return (index + 1);
+}
+
+//caluculation of miliseconds(from -1~1 to 1100~1900)
+int calc_miliseconds(float rawvalue, int midValue, int sensitivity){
+  int ms_temp = midValue + int(sensitivity * rawvalue);
+  if (ms_temp >= 1900) {
+    return 1900;
+  } else {
+    if (ms_temp <= 1100) {
+      return 1100;
+    } else {
+      return ms_temp;
+    }
+  }
+}
+  
 //=====================================================================================//
--- python/XinputRead/main.py
+++ python/XinputRead/main.py
@@ -5,37 +5,49 @@ def main():
   #confirm python3
   try:
     print("readXInput()")
+    readXinput()
   except KeyboardInterrupt:
     print("KeyboardInterrupt")
   finally:
     print("finish")
 
 def readXinput():
+  ser = serial.Serial('COM3', 9600)
+  LEFTvalue_x = 0
+  LEFTvalue_y = 0
+  RIGHTvalue_x = 0
+  RIGHTvalue_y = 0
   while 1:
     #read XInput value
     events = XInput.get_events()
     for event in events:
-      if (event.user_index == 1):
-        if event.type == XInput.EVENT_CONNECTED:
-          print("Controller is successfully connected.\n")  
-            
-        elif event.type == XInput.EVENT_DISCONNECTED:
-          print("Controller is diconnected.\n")
+      if event.type == XInput.EVENT_CONNECTED:
+        print("Controller is successfully connected.\n")  
+          
+      elif event.type == XInput.EVENT_DISCONNECTED:
+        print("Controller is diconnected.\n")
 
-        elif event.type == XInput.EVENT_STICK_MOVED:
-          if event.stick == XInput.LEFT:
-            print("LEFTstickpos: (" + event.x + "," +event.y + ")") 
-            LEFTvalue_x = event.x
-            LEFTvalue_y = event.y
-                
-          elif event.stick == XInput.RIGHT:
-            print("Rightstickpos: (" + event.x + "," +event.y + ")")
-            RIGHTvalue_x = event.x
-            RIGHTvalue_y = event.y
+      elif event.type == XInput.EVENT_STICK_MOVED:
+        if event.stick == XInput.LEFT:
+          #print("LEFTstickpos: (" + str(event.x) + "," + str(event.y) + ")") 
+          LEFTvalue_x = event.x
+          LEFTvalue_y = event.y
+              
+        elif event.stick == XInput.RIGHT:
+          #print("Rightstickpos: (" + str(event.x) + "," + str(event.y) + ")")
+          RIGHTvalue_x = event.x
+          RIGHTvalue_y = event.y
+        buff = str(LEFTvalue_x) + "," + str(LEFTvalue_y) + "," + str(RIGHTvalue_x) + "," + str(RIGHTvalue_y) + "\n"
+        ser.write(buff.encode('utf-8'))
+    
+    if ser.in_waiting>0:
+      serial_value = ser.readline().decode('UTF-8')
+      print("受信したデータ: " + serial_value)
 
-    ser = serial.Serial('COM3', 9600)
-    buff = LEFTvalue_x + "," + LEFTvalue_y + "," + RIGHTvalue_x + "," + RIGHTvalue_y
-    ser.write(buff)
+if __name__ == "__main__":
+  main()
+
+    
 
     
 
